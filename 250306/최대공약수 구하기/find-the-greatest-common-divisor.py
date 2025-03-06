dc6819db009f732dc5089838388cd3214779a5a6
Ji-un Yang <78202408+yangjiun0314@users.noreply.github.com>
2025-03-06 20:13:53 +0900

    Title: 최대공약수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250306/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,11 @@
 def find_a(n, m):
     s = m if m > n else n
-    for i in range(1, s):
+    a = 0
+    for i in range(1, s+1):
         if n%i == 0:
-            
             if m%i == 0:
-                s = i
-    print(s)
+                a = i
+    print(a)
 
 n, m = map(int, input().split())
 
