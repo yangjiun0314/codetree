7a19439c43c808198dceb98104f9b107ad7717c1
Ji-un Yang <78202408+yangjiun0314@users.noreply.github.com>
2025-03-16 15:13:00 +0900

    Title: 함수를 이용한 369 게임, Time: 59ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250316/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250316/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,12 +1,10 @@
 a, b = map(int, input().split())
 
-# Please write your code here.
+# Please write your code here.  
+def game369(n):
+    return ((n%10 == 3) or (n//10 == 3)) or ((n%10 == 6) or (n//10 == 6)) or ((n%10 == 9) or (n//10 == 9)) or ((n%100 == 3) or (n//100 == 3)) or ((n%100 == 6) or (n//100 == 6)) or ((n%100 == 9) or (n//100 == 9))
 def game(n):
     return n%3 == 0 or game369(n)
-    
-def game369(n):
-    return ((n%10 == 3) or (n//10 == 3)) or ((n%10 == 6) or (n//10 == 6)) or ((n%10 == 9) or (n//10 == 9))
-
 cnt = 0
 for i in range(a, b+1):
     if game(i):
