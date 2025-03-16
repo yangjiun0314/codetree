8009a7d371a16860382292b1be691591d9c4ee08
Ji-un Yang <78202408+yangjiun0314@users.noreply.github.com>
2025-03-16 18:23:50 +0900

    Title: 함수를 이용한 369 게임, Time: 100ms, Memory: 18MB, Status: Passed - CodeTree

--- 250316/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250316/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,13 +1,26 @@
 a, b = map(int, input().split())
 
 # Please write your code here.  
-def game369(n):
-    return ((n%10 == 3) or (n//10 == 3)) or ((n%10 == 6) or (n//10 == 6)) or ((n%10 == 9) or (n//10 == 9)) or ((n%100 == 3) or (n//100 == 3)) or ((n%100 == 6) or (n//100 == 6)) or ((n%100 == 9) or (n//100 == 9))
-def game(n):
-    return n%3 == 0 or game369(n)
+def contains_369(n):
+    # 계속 10으로 나눠주며
+    # 일의 자리를 조사합니다.
+    while n > 0:
+        if n % 10 == 3 or n % 10 == 6 or n % 10 == 9:
+            return True
+
+        n //= 10
+
+    return False
+
+
+# 3, 6, 9를 포함하거나 3의 배수인지를 판단합니다.
+def is_369_number(n):
+    return contains_369(n) or (n % 3 == 0)
+
+
 cnt = 0
-for i in range(a, b+1):
-    if game(i):
+for i in range(a, b + 1):
+    if is_369_number(i):
         cnt += 1
 
 print(cnt)
