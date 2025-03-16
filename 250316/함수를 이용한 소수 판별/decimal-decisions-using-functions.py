be6851e337f1575e907a3273ef22160c683e3130
Ji-un Yang <78202408+yangjiun0314@users.noreply.github.com>
2025-03-16 19:02:08 +0900

    Title: 함수를 이용한 소수 판별, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250316/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250316/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -2,12 +2,14 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 def sm(n):
-    for i in range(1, n):
+    for i in range(2, n):
         if n % i == 0:
             return 0
     return n
 
 cnt = 0
 for i in range(a, b+1):
+    if sm(i) == 1:
+        continue
     cnt += sm(i)
 print(cnt)
